b82c2297d957ad6e64d6722ab193ea5b0e6ebee5
JoshHogan99 <52086873+JoshHogan99@users.noreply.github.com>
2024-04-24 00:49:01 +0100

    Finished bar chart and passed FCC tests

--- App.jsx
+++ App.jsx
@@ -56,7 +56,7 @@ export default function App() {
       const svg = d3.select(svgRef.current);
 
       const xScale = d3.scaleTime()
-        .domain(d3.extent(data, d => parseYear(d[0])))
+        .domain([d3.min(data, d => parseYear(d[0])), d3.timeDay.offset(d3.max(data, d => parseYear(d[0])), 75)])
         .range([padding, width - padding]);
         
       const yScale = d3.scaleLinear()
@@ -67,70 +67,67 @@ export default function App() {
       const xAxis = d3.axisBottom(xScale)
       const yAxis = d3.axisLeft(yScale)
 
-      svg.selectAll("rect")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", (d) => xScale(parseYear(d[0])))
-        .attr("y", (d) => yScale(d[1]))
-        .attr("width", 2.5)
-        .attr("height", (d) => height - padding - yScale(d[1]))
-        .attr("fill", "#36aeff")
-        .attr("class", "bar")
-        .on("mouseover", function(event, d) {
-          const year = d[0];
-          const gdp = d[1];
-          handleMouseOver(event, year, gdp);
-        })
-        .on("mouseout", handleMouseOut);
-
-      svg.append("g")
-        .attr("transform", `translate(0, ${height - padding})`)
-        .call(xAxis);
-
-      svg.append("g")
-        .attr("transform", `translate(${padding}, 0)`)
-        .call(yAxis);
       
+      svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => xScale(parseYear(d[0])))
+      .attr("y", (d) => yScale(d[1]))
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1])
+      .attr("width", 2.9)
+      .attr("height", (d) => height - padding - yScale(d[1]))
+      .attr("fill", "#36aeff")
+      .attr("class", "bar")
+      .on("mouseover", function(event, d) {
+        const year = d[0];
+        const gdp = d[1];
+        handleMouseOver(event, year, gdp);
+      })
+      .on("mouseout", handleMouseOut);
+      
+      svg.append("g")
+      .attr("transform", `translate(0, ${height - padding})`)
+      .attr("id", "x-axis")
+      .call(xAxis)
+      .append("text")
+      .attr("x", width - padding)
+      .attr("y", 40)
+      .attr("text-anchor", "end")
+      .attr("fill", "black")
+      .text("More Information: http://www.bea.gov/national/pdf/nipaguid.pdf");
+      
+      svg.append("g")
+      .attr("transform", `translate(${padding}, 0)`)
+      .attr("id", "y-axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", `translate(${padding / 2.5}, ${height / 1.7}) rotate(-90)`)
+      .attr("text-anchor", "start")
+      .attr("fill", "black")
+      .attr("font-size", "1rem")
+      .text("Gross Domestic Product");
+      
+      svg.append("text")
+      .attr("x", width / 2)
+      .attr("y", padding / 2 + 10)
+      .attr("text-anchor", "middle")
+      .text("United States GDP")
+      .attr("id", "title")
+      .attr("font-size", "2rem");
+        
     }, [data])
 
-
-
-  
     return (
       <div id="bar-chart">
-        <h1 id='title'>United States GDP</h1>
         <svg ref={svgRef} width={width} height={height}>
-            {/* {data.map((d, i) => {
-                const year = d[0];
-                const gdp = d[1];
-
-                return (
-                    <g key={i}>
-                        <rect
-                          x={xScale(parseYear(year))} 
-                          y={yScale(gdp)}
-                          width={3.5}
-                          height={2 * gdp}
-                          fill='#36aeff'
-                          className='bar'
-                          onMouseOver={(event) => handleMouseOver(event, year, gdp)}
-                          onMouseOut={handleMouseOut}
-                          >
-                        </rect>
-                    </g>
-                )
-            })} */}
-            {/* <g
-              ref={node => d3.select(node).call(xAxis)}
-            ></g>
-            <g
-              ref={node => d3.select(node).call(yAxis)}
-            ></g> */}
         </svg>
         {tooltipYearText && (
           <div
-            className='tooltip'
+            className='tooltip tick'
+            id="tooltip"
+            data-date={tooltipYearText}
             style={{ 
               left: tooltipPosition.x,
               width: tooltipWidth,
